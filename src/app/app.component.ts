b28f08d294da62791f72dab00185b7b84b134cb6
AlejandroCamba <30796770+AlejandroCamba@users.noreply.github.com>
2019-01-20 03:22:01 -0400

    Revert "Feature prettify formats"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { AuthService } from './shared/services/auth/auth.service';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-
-  	constructor(private authService: AuthService){}
-
-  	isUserLoggedIn(): boolean {
-  		return this.authService.getToken() == null ? false : true;
- 	}
-
- 	ngOnInit() {
- 		/*if (this.authService.getToken()!) {   //<-- comment this code block to remain logged in
-	 		this.authService.logout().subscribe(res => {
-	 			console.log("page reloaded, logging out...");
-	 		});
- 		}*/
- 	}
+export class AppComponent {
+  title = 'frontend-application-test';
 }
